8e518ccfc9bc38c8f93d24041514ec7ccc8fbc9a
Facundo <facundogaitansegura@gmail.com>
2025-08-09 12:51:31 -0300

    Guardado de cambios

--- src/main/java/com/techstore/carrito/Controllers/CarritoController.java
+++ src/main/java/com/techstore/carrito/Controllers/CarritoController.java
@@ -2,4 +2,8 @@ package com.techstore.carrito.Controllers;
 
 public class CarritoController {
 
+    // https://www.baeldung.com/courses/rest-with-spring-boot-course
+    // https://attacomsian.com/blog/spring-data-jpa-one-to-many-mapping
+    // https://tenmilesquare.com/resources/software-development/spring-boot-jpa-relationship-quick-guide/
+
 }
--- src/main/java/com/techstore/carrito/Entities/Producto.java
+++ src/main/java/com/techstore/carrito/Entities/Producto.java
@@ -18,15 +18,17 @@ public class Producto implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
- 
+
     private String nombre;
-    
+
     private Double precio;
 
+    // The @JoinColumn annotation is used to specify the foreign key column in the
+    // relationship owner. The inverse side of the relationship sets the mappedBy
+    // attribute to indicate that the relationship is owned by the other entity.
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "carrito_id", nullable = false)
     private Carrito carrito;
-
 
     public Long getId() {
         return id;
@@ -59,7 +61,5 @@ public class Producto implements Serializable {
     public void setCarrito(Carrito carrito) {
         this.carrito = carrito;
     }
-    
+
 }
-
-
